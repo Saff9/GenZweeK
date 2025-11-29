6907c75d6501e8ba884e06dae00e9284aa4d1e33
Saff9 <saffanakbar942@gmail.com>
2025-11-29 11:31:42 +0530

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,25 @@
+// frontend/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE}/:path*`,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
